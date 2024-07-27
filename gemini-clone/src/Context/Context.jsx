dd68856f82bd05ff63306ca0ba00886902a8e4cc
siddh113 <siddharthketan@gmail.com>
2024-07-27 18:27:17 +0530

    new chat functionality implemented

--- gemini-clone/src/Context/Context.jsx
+++ gemini-clone/src/Context/Context.jsx
@@ -18,6 +18,11 @@ const ContextProvider = (props) => {
         }, 75*index)
     }
 
+    const newChat = () => {
+        setLoading(false);
+        setShowResult(false);
+        }
+
     const onSent = async (prompt) => {
 
         setResultData("")
@@ -67,7 +72,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return (
