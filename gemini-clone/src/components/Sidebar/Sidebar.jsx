129d28a5678653af570ea26fad5071f514818c0e
siddh113 <siddharthketan@gmail.com>
2024-07-27 18:38:38 +0530

    Removed unnecesarry icons

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -48,11 +48,6 @@ const Sidebar = () => {
             </div>
 
             <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" />
-                {extended ? <p>Activity</p> : null}
-            </div>
-
-            <div className="bottom-item recent-entry">
                 <img src={assets.setting_icon} alt="" />
                 {extended? <p>Settings</p> : null}
             </div>
